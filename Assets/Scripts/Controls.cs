ec012dfcf532d01315f85d4e78283e84315fa3cc
Roger Liu <rogliu26@gmail.com>
2015-12-05 16:00:12 -0500

    added materials and dumb controls. Also gave things rigid bodies

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls {
+
+    public static float getFlipperMagnitude()
+    {
+        float flipperMagnitude = Input.GetAxis("Vertical");
+        return flipperMagnitude;
+    }
+
+    public static bool interactInputDown()
+    {
+        return (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"));
+    }
+
+    public static bool lockonInputDown()
+    {
+        return Input.GetKeyDown(KeyCode.LeftShift);
+    }
+
+    public static bool attackInputDown()
+    {
+        return Input.GetMouseButtonDown(1);
+    }
+}
---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperController : MonoBehaviour {
+
+    public float pullBackMagnitude;
+
+	// Use this for initialization
+	void Start () {
+        pullBackMagnitude = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Just moves the flipper up and down lol
+        this.transform.position += new Vector3(0, Controls.getFlipperMagnitude(), 0);
+        //Code that might be used later for accelerometers
+        //transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+	}
+}
