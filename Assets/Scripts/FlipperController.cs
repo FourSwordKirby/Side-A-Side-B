86dbcfec94f27c528d18b495c14012a36876a464
Christine Lee <chris.yx.lee@gmail.com>
2015-12-05 17:03:46 -0500

    flipper rotates back and forth; deleted textures we don't need

--- Assets/Scripts/FlipperController.cs
+++ Assets/Scripts/FlipperController.cs
@@ -5,15 +5,28 @@ public class FlipperController : MonoBehaviour {
 
     public float pullBackMagnitude;
 
+	private Quaternion baseState;
+
+	// Pulls back the flipper
+	void PullBack() {
+		transform.rotation = baseState;
+		transform.RotateAround (transform.position, new Vector3 (0, 1, 0), pullBackMagnitude * 50);
+	}
+
+
 	// Use this for initialization
 	void Start () {
         pullBackMagnitude = 0;
+		baseState = transform.localRotation;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        //Just moves the flipper up and down lol
-        this.transform.position += new Vector3(0, Controls.getFlipperMagnitude(), 0);
+		pullBackMagnitude = Controls.getFlipperMagnitude ();
+		//if (pullBackMagnitude != 0) Debug.Log (pullBackMagnitude);
+
+		PullBack ();
+
         //Code that might be used later for accelerometers
         //transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
 	}
