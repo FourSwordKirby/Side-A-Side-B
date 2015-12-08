1f60bd5a9b1f2d9bfe683b8b7733d6b07a61ae5b
Quanyails <yijingc@andrew.cmu.edu>
2015-12-07 23:42:25 -0500

    Updated my scene with music and controls.

---
+++ Assets/Scripts/Music_Controller.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using System.Collections;
+using MidiJack;
+
+public class Music_Controller : MonoBehaviour {
+
+	AudioSource[] instruments;
+	int instrumentCount = 0; // Will be modifed on start.
+	int keyCount = 128;
+
+	// Use this for initialization
+	void Start ()
+	{
+		AudioSource[] allTracks = this.GetComponentsInChildren<AudioSource> ();
+		int l = allTracks.Length;
+
+		// Filter instruments by tag inefficiently.
+		string tagValue = "Instrument";
+		for (int i = 0; i < l; i++)
+		{
+			instrumentCount += (allTracks[i].tag == tagValue)? 1 : 0;
+		}
+		instruments = new AudioSource[instrumentCount];
+		int instrumentIndex = 0;
+		for (int i = 0; i < l; i++)
+		{
+			AudioSource track = allTracks[i];
+			if (track.tag == tagValue)
+			{
+				instruments[instrumentIndex] = track;
+				instrumentIndex++;
+			}
+		}
+
+		// Set initial volume of instruments to 0.
+		for (int i = 0; i < instrumentCount; i++)
+		{
+			instruments[i].volume = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// FOR USE WITH MIDI INPUT. Switch with keyboard input if necessary.
+		/*
+		for (int key = 0; key < keyCount; key++)
+		{
+			if (MidiMaster.GetKey(key) > 0)
+			{
+				int i = key % instrumentCount;
+				AudioSource instrument = instruments[i];
+				increaseVolume (instrument);
+			}
+		}
+		*/
+
+		// FOR USE WITH KEYBOARD INPUT. Switch with MIDI input if necessary.
+		string keyPressed = Input.inputString;
+		if (keyPressed != "")
+		{
+			int keyOrd = (int)keyPressed [0]; // A bit hacky...
+			int i = keyOrd % instrumentCount;
+			AudioSource instrument = instruments[i];
+			increaseVolume (instrument);
+		}
+
+		// DO NOT TOGGLE THIS.
+		for (int i = 0; i < instrumentCount; i++)
+		{
+			decreaseVolume (instruments[i]);
+		}
+	}
+
+	void changeVolume(AudioSource audio, float amount)
+	{
+		float currentVolume = audio.volume;
+		float newVolume = currentVolume + amount;
+		if (newVolume > 1F) {
+			newVolume = 1F;
+		} else if (newVolume < 0F) {
+			newVolume = 0F;
+		}
+		audio.volume = newVolume;
+//		Debug.Log ("Changed " + audio + " to " + newVolume);
+	}
+	
+	float increaseAmount = 0.08F;
+	void increaseVolume(AudioSource audio)
+	{
+		changeVolume (audio, increaseAmount);
+		//		Debug.Log ("Increasing " + audio);
+	}
+	
+	float decreaseAmount = 0.005F;
+	void decreaseVolume(AudioSource audio)
+	{
+		changeVolume (audio, -decreaseAmount);
+		//		Debug.Log ("Decreasing " + audio);
+	}
+}
