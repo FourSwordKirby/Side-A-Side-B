f47425192aa66b77151dab68be5a00f5fd87a72c
Chris Lee <chris.yx.lee@gmail.com>
2015-12-13 15:18:32 -0500

    added 6 notes in a pentatonic scale and moved all sounds to resource folder

--- Assets/Scripts/MusicBall.cs
+++ Assets/Scripts/MusicBall.cs
@@ -3,16 +3,51 @@ using System.Collections;
 
 public class MusicBall : MonoBehaviour {
 
+    /* Side A (pond)
+     *  piano, drum, guitar, clarinet 
+     * Side B (casino)
+     */
+    public string note;
+    public string[] soundsA; //pond
+    public string[] soundsB; //casino
+
 	// Use this for initialization
 	void Start () {
-	
+        soundsA = new string[4];
+        soundsA[0] = "Clarinet";
+        soundsA[1] = "Drum";
+        soundsA[2] = "Guitar";
+        soundsA[3] = "Piano";
+
+        soundsB = new string[6];
+        soundsB[0] = "Db4";
+        soundsB[1] = "Eb4";
+        soundsB[2] = "F4";
+        soundsB[3] = "Ab4";
+        soundsB[4] = "Bb4";
+        soundsB[5] = "Db5";
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (this.transform.position.y <= -10)
             this.transform.position = new Vector3(Random.Range(-4, 4),40, Random.Range(-4, 4));
+        if (note == null) {
+            //TODO: initialize with the side that this ball is on
+        }
 	}
+
+    void Initialize(bool sideA) {
+
+
+
+        if (sideA) {
+
+        } else {
+
+        }
+
+    }
 
     void OnCollisionEnter(Collision col)
     {
