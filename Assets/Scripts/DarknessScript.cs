5f6f84a09cc3bbb79c89c7f8301df729656e0b29
Roger Liu <rogliu26@gmail.com>
2015-12-14 18:47:34 -0500

    added script for darkness

---
+++ Assets/Scripts/DarknessScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarknessScript : MonoBehaviour {
+
+    public float transparency = 1.0f;
+    private bool isDark;
+
+	// Use this for initialization
+	void Start () {
+        isDark = false;
+        transparency = 0.0f;
+        Color oldColor = GetComponent<Renderer>().material.color;
+        Color newColor = new Color(oldColor.r, oldColor.b, oldColor.g, transparency);
+        GetComponent<Renderer>().material.SetColor("_Color", newColor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isDark)
+        {
+            if (transparency > 0.0f)
+            {
+                transparency -= 0.02f;
+                Color oldColor = GetComponent<Renderer>().material.color;
+                Color newColor = new Color(oldColor.r, oldColor.b, oldColor.g, transparency);
+                GetComponent<Renderer>().material.SetColor("_Color", newColor);
+            }
+        }
+        else{
+            if (transparency < 1.0f)
+            {
+                transparency += 0.02f;
+                Color oldColor = GetComponent<Renderer>().material.color;
+                Color newColor = new Color(oldColor.r, oldColor.b, oldColor.g, transparency);
+                GetComponent<Renderer>().material.SetColor("_Color", newColor);
+            }
+        }
+	}
+
+    void turnDark()
+    {
+        isDark = true;
+    }
+
+    void turnBright()
+    {
+        isDark = false;
+    }
+}
